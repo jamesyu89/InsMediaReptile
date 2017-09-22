dd6b7adb12bf4c0916abc2f69f609b160f3343ee
邦 余 <360031914@qq.com>
2017-09-22 19:28:25 +0800

    --余邦 优化一下窗体布局

--- InstagramPhotosCollection/Form1.Designer.cs
+++ InstagramPhotosCollection/Form1.Designer.cs
@@ -47,11 +47,11 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.richTextBox2, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(-1, 122);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(24, 129);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(986, 612);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(936, 609);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // richTextBox1
@@ -60,7 +60,7 @@
             this.richTextBox1.Location = new System.Drawing.Point(3, 3);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(487, 606);
+            this.richTextBox1.Size = new System.Drawing.Size(462, 603);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
@@ -68,17 +68,17 @@
             // richTextBox2
             // 
             this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox2.Location = new System.Drawing.Point(496, 3);
+            this.richTextBox2.Location = new System.Drawing.Point(471, 3);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.ReadOnly = true;
-            this.richTextBox2.Size = new System.Drawing.Size(487, 606);
+            this.richTextBox2.Size = new System.Drawing.Size(462, 603);
             this.richTextBox2.TabIndex = 1;
             this.richTextBox2.Text = "";
             this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(24, 8);
+            this.button1.Location = new System.Drawing.Point(24, 21);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(465, 34);
             this.button1.TabIndex = 1;
@@ -88,7 +88,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(495, 9);
+            this.button2.Location = new System.Drawing.Point(495, 22);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(465, 34);
             this.button2.TabIndex = 2;
@@ -100,7 +100,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(21, 96);
+            this.label1.Location = new System.Drawing.Point(21, 109);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(470, 17);
             this.label1.TabIndex = 3;
@@ -110,7 +110,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(494, 96);
+            this.label2.Location = new System.Drawing.Point(494, 109);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(470, 17);
             this.label2.TabIndex = 4;
@@ -118,7 +118,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(24, 57);
+            this.button3.Location = new System.Drawing.Point(24, 70);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(465, 34);
             this.button3.TabIndex = 5;
@@ -128,7 +128,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(495, 57);
+            this.button4.Location = new System.Drawing.Point(495, 70);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(465, 34);
             this.button4.TabIndex = 6;
@@ -140,7 +140,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(989, 737);
+            this.ClientSize = new System.Drawing.Size(989, 755);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
